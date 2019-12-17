f09561957a45f6e4e32c38ffe6100696d7dccd21
Jun Chandra <cjun580@gmail.com>
2019-12-17 18:53:03 +0700

    login done

--- src/Component/LoginForm.jsx
+++ src/Component/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LoginForm.css'
+import google from '../image/google.jpg'
 
 export default class LoginForm extends React.Component{
     render(){
@@ -8,10 +9,18 @@ export default class LoginForm extends React.Component{
                 <form>
                     <h2>Log in to your account</h2>
                     <input placeholder="Username"></input>
-                    <input placeholder="Password" type="password"></input>
+                    <input className = "passwordField" placeholder="Password" type="password"></input>
                     <button type="submit">Log In</button>
-                    <span>Forgot Password?</span>
+                    <span className="Forgot">Forgot Password?</span>
                 </form>
+                <div className="register">
+                    <img className = "google" src={google} alt=""/>
+                    <p className="googleLogin">Log In with Google</p>
+                </div>
+                <div className="register">
+                    <p>Don't have an account?</p>
+                    <p className="signUp">Sign Up Here</p>
+                </div>
             </div>
         )
     }
